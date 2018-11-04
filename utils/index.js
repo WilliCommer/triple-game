9fb054f5811272d63da455cfdd2e905d4c1980f8
Willi Commer <willicommer@gmail.com>
2018-11-04 19:39:44 +0100

    Delete index.js

--- utils/index.js
+++
@@ -1,4 +0,0 @@
-module.exports.percentageRandom       = require('./percentage-random.js');
-module.exports.getProp                = require('./get-prop.js').getProp;
-module.exports.checkProp              = require('./get-prop.js').checkProp;
-
